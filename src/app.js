837fe339edb91888143703055d0ec240862996d1
mohit0224 <mohitdheer24+git@gmail.com>
2024-03-12 18:23:19 +0530

    setup express json, urlencoded, static files config and cookieparser config

--- src/app.js
+++ src/app.js
@@ -3,11 +3,24 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
+// setup cors config
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
+
+// setup express json, urlencoded, static files config and cookieparser config
+
+app.use(express.json({ limit: "20kb" }));
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "20kb",
+  })
+);
+app.use(express.static("public"));
+app.use(cookieParser());
 
 export { app };
