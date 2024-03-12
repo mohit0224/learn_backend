678d236d6ddb9b04895b39f76c6e30fa33a07187
mohit0224 <mohitdheer24+git@gmail.com>
2024-03-12 17:50:39 +0530

    setup express server

--- src/db/index.js
+++ src/db/index.js
@@ -6,10 +6,14 @@ const dbConnection = async () => {
     const { connection } = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
-    console.log(`database connected to ${connection.host} !!`);
+    console.log(
+      `database connected successfully on host :: ${connection.host} !!`
+    );
   } catch (error) {
-    console.log(`Error connecting to database :: ${error}`);
-    process.exit(1);
+    throw error.message;
+
+    // console.log(`Error connecting to database :: ${error}`);
+    // process.exit(1);
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import dotenv from "dotenv";
 import dbConnection from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
 
-dbConnection();
+dbConnection()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`⚙️ server is running at :: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`Error connecting to database :: ${error}`);
+    process.exit(1);
+  });
