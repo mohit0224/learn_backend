f6949d12e47bc3eac85a23734751866536db854b
mohit0224 <mohitdheer24+git@gmail.com>
2024-03-13 17:57:03 +0530

    setup jwt in user.model.js = for create user access token and refresh token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema(
       lowercase: true,
       trim: true,
     },
-    fullname: {
+    fullName: {
       type: String,
       required: true,
       trim: true,
@@ -49,20 +49,49 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// ? how to encrypt the password
+// ! how to encrypt the password
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) next();
-  // ! this line of code check the password field is filled by user or not.
-  // !If user fill the field then execute next line of code otherwise return next.
+  // ? this line of code check the password field is filled by user or not.
+  // ? If user fill the field then execute next line of code otherwise return next.
 
-  this.password = await bcrypt.hash(this.password, 10); // ! this code encrypt the password
+  this.password = await bcrypt.hash(this.password, 10); // ? this code encrypt the password
   next();
 });
 
-// ? how to decrypt the password
-// ! schema.methos allows to create new methods to do some functionality
+// ! how to decrypt the password
+// ? schema.methos allows to create new methods to do some functionality
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
+};
+
+// ! how to create access tokens and refresh tokens
+// ? create new mongoose method to access token
+userSchema.methods.generateAccessToken = function () {
+  Jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+// ? create new mongoose method to refresh token
+userSchema.methods.generateRefreshToken = function () {
+  Jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
 };
 
 const User = mongoose.model("User", userSchema);
