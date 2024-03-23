aa94c1a4fac4e10e2ad8177564d9e10c1c7a9ab0
mohit0224 <mohitdheer24+git@gmail.com>
2024-03-23 13:58:28 +0530

    fix subscriptions model

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -4,11 +4,11 @@ const subscriptionSchema = new Schema(
     {
         subscriber: {
             type: Schema.Types.ObjectId,
-            ref: User,
+            ref: "User",
         },
         channel: {
             type: Schema.Types.ObjectId,
-            ref: User,
+            ref: "User",
         },
     },
     { timestamps: true }
